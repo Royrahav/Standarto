38504c7e4f3239b3d99f82f603d850f0db8a621a
Royrahav <roy.r770@gmail.com>
2022-06-10 17:24:07 +0300

    Now the code produces the DB to CSV

--- Jobs/DataService.py
+++ Jobs/DataService.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from Jobs.NightJob import NightJob
+import numpy as np
+
 
 class DataService:
 
@@ -7,7 +9,11 @@ class DataService:
         nj = NightJob()
         self.m_ratings_df, self.m_basics_df = nj.act()
         self.m_ratings_df = self.m_ratings_df[self.m_ratings_df['numVotes'] > 10000]
-        m_main_df = DataFrame({"title_id": [], "title": [], "rating": [], "numVotes": []})
+        self.m_basics_df = self.m_basics_df[self.m_basics_df['titleType'] == 'movie']
+        self.m_main_df = self.m_ratings_df.merge(self.m_basics_df, how="inner", on="tconst")
+        print(self.m_main_df[['primaryTitle', 'startYear', 'averageRating']])
+
+        self.m_main_df.to_csv(r'E:\Roy\Other\readme.txt', header=None, index=None, sep='\t', mode='a')
 
     def get_basics_df(self):
         return self.m_basics_df
