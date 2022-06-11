a3b2dd1385aa43574d444752a554a8d4946dece5
Royrahav <roy.r770@gmail.com>
2022-06-11 11:33:37 +0300

    SomeData Services Improvments

--- Jobs/DataService.py
+++ Jobs/DataService.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from Jobs.NightJob import NightJob
-import numpy as np
+import os
 
 
 class DataService:
@@ -8,18 +8,38 @@ class DataService:
     def __init__(self):
         nj = NightJob()
         self.m_ratings_df, self.m_basics_df = nj.act()
-        self.m_ratings_df = self.m_ratings_df[self.m_ratings_df['numVotes'] > 10000]
-        self.m_basics_df = self.m_basics_df[self.m_basics_df['titleType'] == 'movie']
-        self.m_main_df = self.m_ratings_df.merge(self.m_basics_df, how="inner", on="tconst")
-        print(self.m_main_df[['primaryTitle', 'startYear', 'averageRating']])
-
-        self.m_main_df.to_csv(r'E:\Roy\Other\readme.txt', header=None, index=None, sep='\t', mode='a')
+        self.filter_data_frames()
+        self.create_main_data_frame()
 
     def get_basics_df(self):
         return self.m_basics_df
 
     def get_ratings_df(self):
         return self.m_ratings_df
+
+    def get_main_dataframe(self):
+        return self.m_main_df
+
+    def filter_data_frames(self):
+        self.m_ratings_df = self.m_ratings_df[self.m_ratings_df['numVotes'] > 10000]
+        self.m_basics_df = self.m_basics_df[self.m_basics_df['titleType'] == 'movie']
+
+    def create_main_data_frame(self):
+        self.m_main_df = self.m_ratings_df.merge(self.m_basics_df, how="inner", on="tconst")
+        self.m_main_df.drop(['titleType', 'originalTitle', 'isAdult', 'endYear', 'runtimeMinutes'], axis=1,
+                            inplace=True)
+        self.m_main_df.sort_values(by=['numVotes'], ascending=False, inplace=True)
+
+    def create_csv_for_testing(self):
+        if os.path.exists(r'E:\Roy\Other\readme.txt'):
+            os.remove(r'E:\Roy\Other\readme.txt')
+
+        f = open(r'E:\Roy\Other\readme.txt', "a")
+        for key in self.m_main_df.keys():
+            f.write(key + '\t')
+        f.write('\n')
+        f.close()
+        self.m_main_df.to_csv(r'E:\Roy\Other\readme.txt', header=None, index=None, sep='\t', mode='a')
 
     # dataframes
     m_basics_df = pd.DataFrame()
